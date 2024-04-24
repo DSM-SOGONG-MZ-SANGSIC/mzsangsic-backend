e78ec9ca1cb976f5140ebac1790b6fc6b2a46951
sim <simyechan87@gmail.com>
2024-04-24 11:58:07 +0900

    🧩 :: 문제 저장 api

--- src/main/java/com/geunoo/mzsangsicbackend/domain/quiz/controller/QuizController.java
+++ src/main/java/com/geunoo/mzsangsicbackend/domain/quiz/controller/QuizController.java
@@ -3,6 +3,7 @@ package com.geunoo.mzsangsicbackend.domain.quiz.controller;
 import com.geunoo.mzsangsicbackend.domain.quiz.controller.dto.request.QuizRequest;
 import com.geunoo.mzsangsicbackend.domain.quiz.controller.dto.response.QuizResponse;
 import com.geunoo.mzsangsicbackend.domain.quiz.service.SolveQuizService;
+import com.geunoo.mzsangsicbackend.domain.quiz.service.UserQuizService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,11 +14,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/quiz")
 public class QuizController {
 
-    private final SolveQuizService quizSolvingService;
+    private final SolveQuizService solveQuizService;
+    private final UserQuizService userQuizService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{quiz-id}")
     public QuizResponse solvingQuiz(@PathVariable("quiz-id") Long quizId, @RequestBody @Valid QuizRequest request) {
-        return quizSolvingService.execute(quizId, request);
+        return solveQuizService.execute(quizId, request);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/user/{quiz-id}")
+    public void saveQuiz(@PathVariable("quiz-id") Long quizId) {
+        userQuizService.execute(quizId);
     }
 }
--- src/main/java/com/geunoo/mzsangsicbackend/domain/quiz/service/UserQuizService.java
+++ src/main/java/com/geunoo/mzsangsicbackend/domain/quiz/service/UserQuizService.java
@@ -1,10 +1,9 @@
 package com.geunoo.mzsangsicbackend.domain.quiz.service;
 
-import com.geunoo.mzsangsicbackend.domain.quiz.controller.dto.request.QuizRequest;
-import com.geunoo.mzsangsicbackend.domain.quiz.controller.dto.response.QuizResponse;
-import com.geunoo.mzsangsicbackend.domain.quiz.entity.*;
-import com.geunoo.mzsangsicbackend.domain.quiz.entity.repository.SolvedQuizRepository;
+import com.geunoo.mzsangsicbackend.domain.quiz.entity.Quiz;
+import com.geunoo.mzsangsicbackend.domain.quiz.entity.UserQuiz;
 import com.geunoo.mzsangsicbackend.domain.quiz.entity.repository.QuizRepository;
+import com.geunoo.mzsangsicbackend.domain.quiz.entity.repository.UserQuizRepository;
 import com.geunoo.mzsangsicbackend.domain.user.entity.User;
 import com.geunoo.mzsangsicbackend.global.error.exceptions.NotFoundException;
 import com.gil.easyjwt.user.CurrentUserService;
@@ -16,24 +15,19 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class SolveQuizService {
+public class UserQuizService {
 
     private final QuizRepository quizRepository;
+    private final UserQuizRepository userQuizRepository;
     private final CurrentUserService<User> currentUserService;
-    private final SolvedQuizRepository solvedQuizRepository;
 
     @Transactional
-    public QuizResponse execute(Long quizId, QuizRequest request) {
+    public void execute(Long quizId) {
         Optional<Quiz> optionalQuiz = quizRepository.findById(quizId);
         Quiz quiz = optionalQuiz.orElseThrow(() -> new NotFoundException("문제를 찾지 못했습니다."));
         User user = currentUserService.getCurrentUser();
-        Pick pick = new Pick(quiz.getContent(), quiz);
 
-        solvedQuizRepository.save(new SolvedQuiz(quiz, pick, user));
+        userQuizRepository.save(new UserQuiz(user, quiz));
 
-        return QuizResponse.builder()
-                .answer(quiz.getAnswer().equals(request.getPickId()))
-                .commentation(quiz.getCommentation())
-                .build();
     }
 }
