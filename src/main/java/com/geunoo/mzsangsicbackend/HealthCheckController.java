74cc3dd11e0adf124a6a8bb9d07e9a6228bdf1f7
길근우 <kesksjxhx@gmail.com>
2024-05-01 22:33:48 +0900

    🧩 :: health check api 추가

---
+++ src/main/java/com/geunoo/mzsangsicbackend/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.geunoo.mzsangsicbackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping
+    public String check() {
+        return "OK";
+    }
+}
