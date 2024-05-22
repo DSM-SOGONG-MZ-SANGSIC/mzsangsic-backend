d91cab345f403ad2f08a018e3e31e34e5a64fa87
sim <simyechan87@gmail.com>
2024-05-22 19:34:39 +0900

    ⚙️ :: pick 엔티티 저장하도록 수정

--- src/main/java/com/geunoo/mzsangsicbackend/domain/quiz/service/SolveQuizService.java
+++ src/main/java/com/geunoo/mzsangsicbackend/domain/quiz/service/SolveQuizService.java
@@ -3,6 +3,7 @@ package com.geunoo.mzsangsicbackend.domain.quiz.service;
 import com.geunoo.mzsangsicbackend.domain.quiz.controller.dto.request.QuizRequest;
 import com.geunoo.mzsangsicbackend.domain.quiz.controller.dto.response.AnswerResponse;
 import com.geunoo.mzsangsicbackend.domain.quiz.entity.*;
+import com.geunoo.mzsangsicbackend.domain.quiz.entity.repository.PickRepository;
 import com.geunoo.mzsangsicbackend.domain.quiz.entity.repository.SolvedQuizRepository;
 import com.geunoo.mzsangsicbackend.domain.quiz.entity.repository.QuizRepository;
 import com.geunoo.mzsangsicbackend.domain.user.entity.User;
@@ -20,6 +21,7 @@ public class SolveQuizService {
     private final QuizRepository quizRepository;
     private final CurrentUserService<User> currentUserService;
     private final SolvedQuizRepository solvedQuizRepository;
+    private final PickRepository pickRepository;
 
     @Transactional
     public AnswerResponse execute(Long quizId, QuizRequest request) {
@@ -27,6 +29,7 @@ public class SolveQuizService {
         Quiz quiz = quizRepository.findById(quizId)
                 .orElseThrow(() -> new NotFoundException("문제를 찾지 못했습니다."));
         Pick pick = new Pick(quiz.getContent(), quiz);
+        pickRepository.save(pick);
 
         solvedQuizRepository.save(new SolvedQuiz(quiz, pick, user));
 
